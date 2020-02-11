4df4ff2b17dbe3d7811581a7f9b55e1686706ea7
Oscar <42596513+oscarjohnston@users.noreply.github.com>
2020-02-10 17:36:38 -0700

    Added Player Input Script
    Added a script to take in player input when working wtih an xbox controller.

---
+++ Lockbright/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float SPEED = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float VerticalTranslation = Input.GetAxis("Vertical") * SPEED;
+        float HorizontalTranslation = Input.GetAxis("Horizontal") * SPEED;
+
+        VerticalTranslation *= Time.deltaTime;
+        HorizontalTranslation *= Time.deltaTime;
+
+        transform.Translate(HorizontalTranslation, 0, VerticalTranslation);
+    }
+}
