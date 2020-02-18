3e46f1284945c7f4fc77f0ff6bed8ec7dfddeeec
oscarjohnston <42596513+oscarjohnston@users.noreply.github.com>
2020-02-18 13:44:02 -0700

    Fixed Game Movement to by physics based

--- Lockbright/Assets/Scripts/PlayerInput.cs
+++ Lockbright/Assets/Scripts/PlayerInput.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour
 {
-    public float SPEED = 10.0f;
+    public float SPEED = 1.0f;
 
     public string PlayerNumber = "1";
+
+    public Image SpeechBubble;
+    public Text BubbleText;
+
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ToggleSpeechBubble(false);
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,6 +30,32 @@ public class PlayerInput : MonoBehaviour
         VerticalTranslation *= Time.deltaTime;
         HorizontalTranslation *= Time.deltaTime;
 
-        transform.Translate(HorizontalTranslation, VerticalTranslation, 0);
+        //transform.Translate(HorizontalTranslation, VerticalTranslation, 0);
+        //rb2d.MovePosition(new Vector2(HorizontalTranslation, VerticalTranslation));
+        rb2d.velocity = new Vector2(HorizontalTranslation, VerticalTranslation);
     }
+
+    /// <summary>
+    /// Toggles the speech bubble that goes over the player.
+    /// </summary>
+    /// <param name="changeTo">The state you want the bubble to be</param>
+    private void ToggleSpeechBubble(bool changeTo)
+    {
+        SpeechBubble.enabled = changeTo;
+        BubbleText.enabled = changeTo;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Colliding!");
+
+        ToggleSpeechBubble(true);
+        BubbleText.text = "Life is pain";
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        ToggleSpeechBubble(false);
+    }
+    
 }
