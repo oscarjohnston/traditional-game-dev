c61c90cbddf041775cacb8696c616b1949409092
oscarjohnston <42596513+oscarjohnston@users.noreply.github.com>
2020-03-05 12:15:01 -0700

    Resume and Quit buttons work

--- Lockbright/Assets/PauseMenu.cs
+++ Lockbright/Assets/PauseMenu.cs
@@ -24,7 +24,7 @@ public class PauseMenu : MonoBehaviour
             }
         }
     }
-    private void Resume()
+    public void Resume()
     {
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
@@ -36,6 +36,11 @@ public class PauseMenu : MonoBehaviour
         PauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
 }
