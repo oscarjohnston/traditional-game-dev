d969d7cbf344724ad11fdc0aa75b3be97a0f2489
Oscar <42596513+oscarjohnston@users.noreply.github.com>
2020-03-21 11:29:55 -0600

    Pause Button Pauses the Game

--- Lockbright/Assets/PauseMenu.cs
+++ Lockbright/Assets/PauseMenu.cs
@@ -9,20 +9,34 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject PauseMenuUI;
 
+    // This variable is so only one player can control the pause screen at one time
+    private int PlayerWhoPaused;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // This section is for an unpaused game
+        if (!GameIsPaused)
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
+            // Check if it's time to pause the game
+            if (StartButtonIsPressedToPause() || Input.GetKeyDown(KeyCode.Escape))
             {
                 Pause();
+                return;
             }
         }
+
+        // This section is for a paused game
+        else
+        {
+            // Check if it's time to play the game again by the player who paused
+            if (Input.GetButtonDown("Start_Button_" + PlayerWhoPaused) || Input.GetKeyDown(KeyCode.Escape))
+            {
+                Resume();
+                return;
+            }
+        }
+        
     }
     public void Resume()
     {
@@ -41,6 +55,35 @@ public class PauseMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    /// <summary>
+    /// Helper method for checking if any of the controllers pressed the start button, then recording who did
+    /// </summary>
+    /// <returns></returns>
+    private bool StartButtonIsPressedToPause()
+    {
+        if (Input.GetButtonDown("Start_Button_1"))
+        {
+            PlayerWhoPaused = 1;
+            return true;
+        }
+        else if (Input.GetButtonDown("Start_Button_2"))
+        {
+            PlayerWhoPaused = 2;
+            return true;
+        }
+        else if (Input.GetButtonDown("Start_Button_3"))
+        {
+            PlayerWhoPaused = 3;
+            return true;
+        }
+        else if (Input.GetButtonDown("Start_Button_4"))
+        {
+            PlayerWhoPaused = 4;
+            return true;
+        }
+        else { return false; }
     }
 
 }
