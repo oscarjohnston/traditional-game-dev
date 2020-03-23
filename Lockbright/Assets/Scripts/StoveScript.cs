a85386901b24f5f4b0aa74077d677e43efb3b00b
kylegeorgi <46452747+kylegeorgi@users.noreply.github.com>
2020-03-22 21:18:41 -0600

    Revert "Kyle"
    This reverts commit ec02bf7e212821d47c26e0116f015ed81d8b8bb9.

--- Lockbright/Assets/Scripts/StoveScript.cs
+++ Lockbright/Assets/Scripts/StoveScript.cs
@@ -1,29 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class StoveScript : MonoBehaviour
 {
     // Controller for particle
     public UIController game;
 
-    // Boolean for identifying if the stove is on
+    // Bollean for identifying if the stove is on
     private bool StoveIsLit;
-
-    // Required Objects
-    public GameObject PaleTonic;
-    public GameObject LivingMossFlower;
-    public GameObject Pomegranate;
-
-    // Number of items still required
-    public int itemsleft = 3;
-
-    // Interaction helpers
-    public GameObject reward;
-    public bool spawned;
-    public string RewardText;
-    public string InteractionText;
 
     void Start()
     {
@@ -60,69 +45,6 @@ public class StoveScript : MonoBehaviour
 
                 print("Turning off stove");
             }
-        }
-    }
-    public void TryToInteractWithThisObject(string Class, ref GameObject HeldItem, ref Text BubbleText, ref bool grabbed)
-    {
-        // No items added gives first message
-        if (itemsleft == 3)
-        {
-            BubbleText.text = InteractionText;
-        }
-        // Otherwise gives this message
-        else
-        {
-            BubbleText.text = "Seems like we're missing something...";
-        }
-
-        // Check if this object has spawned its item yet
-        if (!spawned)
-        {
-            // Is the player holding one of the right items? (and optionally the correct Player?)
-            if (HeldItem == PaleTonic)
-            {
-                itemsleft--;
-
-                BubbleText.text = "Good job! You only need " + itemsleft + "more items it seems";
-
-                Destroy(HeldItem);
-
-                grabbed = false;
-            }
-            if (HeldItem == Pomegranate)
-            {
-                itemsleft--;
-
-                BubbleText.text = "Good job! You only need " + itemsleft + "more items it seems";
-
-                Destroy(HeldItem);
-
-                grabbed = false;
-            }
-            if (HeldItem == LivingMossFlower)
-            {
-                itemsleft--;
-
-                BubbleText.text = "Good job! You only need " + itemsleft + "more items it seems";
-
-                Destroy(HeldItem);
-
-                grabbed = false;
-            }
-
-
-            if (reward != null && itemsleft == 0 && StoveIsLit == true)
-            {
-                HeldItem = reward;
-                grabbed = true;
-                BubbleText.text = RewardText;
-            }
-            else
-            {
-                grabbed = false;
-            }
-            //Instantiate(reward, spawnPoint, new Quaternion(0, 0, 0, 0));
-            spawned = true;
         }
     }
 }
